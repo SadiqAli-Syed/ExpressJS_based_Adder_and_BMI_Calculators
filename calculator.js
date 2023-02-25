9bb996dc5715641f9a80896d80d83aef7d294869
Syed Sadiq Ali <89778344+SadiqAli-Syed@users.noreply.github.com>
2023-02-25 11:18:05 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -14,8 +14,12 @@ app.post("/", (req, res)=> {
     const var2 = parseInt(req.body.num2, 10);
     const var3 = var1 + var2;
     console.log(var3);
+    const html = 
+    '<h1 style = "color:aqua;background-color:rgb(42, 42, 42);text-align:center;padding:50vh 0;font-size:3rem;font-family: Arial, sans-serif;">"The result is logged in your console"</h1>'
+
+
     
-    res.send("The result is "+var3);
+    res.send(html);
 })
 
 app.listen(3000)
