c1676da8cbd795fcc5f12024244230ccead823cb
Syed Sadiq Ali <89778344+SadiqAli-Syed@users.noreply.github.com>
2023-02-25 14:24:37 +0530

    Add files via upload
    Improved Appearance and output Functionality.

--- calculator.js
+++ calculator.js
@@ -10,16 +10,19 @@ app.get("/",(req, res) =>{
 })
 
 app.post("/", (req, res)=> {
-    const var1 = parseInt(req.body.num1, 10);
-    const var2 = parseInt(req.body.num2, 10);
+    const var1 = parseFloat(req.body.num1, 10);
+    const var2 = parseFloat(req.body.num2, 10);
     const var3 = var1 + var2;
-    console.log(var3);
-    const html = 
-    '<h1 style = "color:aqua;background-color:rgb(42, 42, 42);text-align:center;padding:50vh 0;font-size:3rem;font-family: Arial, sans-serif;">"The result is logged in your console"</h1>'
+    console.log("Result, \n" + var1 + " + " + var2 + " = " + var3);
+    const result = '<h2 style="color:aqua;text-align:center;padding:4.5vh 0;font-size:3rem;">Result = ' + var3 + '</h2>';
+    const cons_l = 
+    '<style>html{;text-align:center;font-family: Arial;background-color:rgb(32, 32, 32);}</style><h1 style ="color:lightgreen" >"The result is logged into your console aswell"</h1>'
+    const button =
+    '<form action="/" method="get"><button style=";background-color: rgb(255, 223, 176);border-radius: 4px;height:2.5rem;display:block; margin: 0 auto 50vh;" type="submit">Calculate More</button></form>';
 
 
     
-    res.send(html);
+    res.send(result +  button + cons_l);
 })
 
 app.listen(3000);
